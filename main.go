74990a898035e54ac6b061cfc76e5bc46e143ecb
Dan Finneran <daniel.finneran@gmail.com>
2023-01-18 12:16:20 +0000

    Add comments to example
    Signed-off-by: Dan Finneran <daniel.finneran@gmail.com>

--- main.go
+++ main.go
@@ -7,18 +7,27 @@ import (
 )
 
 func main() {
+	// Create a new XDP object (name, license, debugging)
 	x, err := ebpf.NewXDP("Demo", "GPL", true)
 	if err != nil {
 		log.Fatalf("Error creating XDP object: %s", err)
 	}
+	// Enable TCP parsing (including adding headers)
 	x.ParseTCP()
+
+	// Get some variables!
 	x.GetTCPDestinationPort()
 	x.GetTCPSourcePort()
 	x.GetIPDestinationAddress()
 	x.GetIPSourceAddress()
+
+	// Add some code
 	x.AppendCode("bpf_printk(\"from %pI4, to %pI4:%d\", &saddress, &daddress, bpf_htons(dport));")
+
+	// Create the code in-memory
 	x.Create()
 
+	// Generate the code/create the Go Wrappers, and if true create a Go snippet
 	err = x.Generate(true)
 	if err != nil {
 		panic(err)
